5ffe3bb1f5369e5a367e1767455f5e888854dfc3
owarner <owarner@ceiamerica.com>
2022-10-26 12:27:40 -0400

    added angular material mods.. added service

--- weather-app/src/app/app.component.ts
+++ weather-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'weather-app';
+
+  cities = ["London", "Paris", "Moscow", "New York", "Karachi", "Sydney"];
+
+  cityControl: FormControl = new FormControl;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.cityControl = new FormControl("");
+    this.cityControl.valueChanges
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+  }
+
+  ngOnDestroy() {
+  }
 }
