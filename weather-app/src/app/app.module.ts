66f742a8dea1530f3f3d4a6de4b02afcde7f4d97
owarner <owarner@ceiamerica.com>
2022-10-26 14:21:02 -0400

    created weather report component

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -8,10 +8,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSelectModule} from '@angular/material/select';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
+import { WeatherReportComponent } from './components/weather-report/weather-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherReportComponent
   ],
   imports: [
     BrowserModule,
