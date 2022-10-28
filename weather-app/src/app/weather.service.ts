00339da15c67ab47b881d52c4ede266a290c12c0
owarner <owarner@ceiamerica.com>
2022-10-28 09:06:11 -0400

    code cleanup

--- weather-app/src/app/weather.service.ts
+++ weather-app/src/app/weather.service.ts
@@ -6,12 +6,15 @@ import { delay } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
+
 export class WeatherService {
 
   constructor(private http: HttpClient) {}
 
+  //returns weather data based on selected city
   getWeatherForCity(city: string): Observable<any> {
     const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a0cabcbef29ea56b81f73cbc8a40cca1&units=imperial`;
+    //delay 500 in order to show progress loading bar
     return this.http.get(path).pipe(delay(500));
   }
 
